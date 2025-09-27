b22fdf3cb9122a23931c4eda4e8091d2a82f3ed4
Wania Irfan <waniaanjumm@gmail.com>
2025-09-27 15:08:59 +0500

    innitail commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Navbar scroll effect
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("shadow-lg");
+  } else {
+    navbar.classList.remove("shadow-lg");
+  }
+});
